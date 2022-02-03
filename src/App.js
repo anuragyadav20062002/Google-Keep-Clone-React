7622a628a42551e9e86b3f12e9abd99fa20a8b7c
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-02-03 20:44:49 +0530

    created footer and new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
+import CreateNote from "./Components/CreateNote"
 
 const App = () => {
   return (
     <>
       <Header />
       <Footer />
+      <CreateNote />
     </>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,9 @@
 import React from "react"
-import logo from "./Images/logo.png"
 
 const Header = () => {
   return (
     <>
       <div className="header">
-        <img src={logo} alt="logo" width="70" height="70" />
         <h1>Keep Clone</h1>
       </div>
     </>
