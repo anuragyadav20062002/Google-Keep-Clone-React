e4d960a91629a93bf1efc09ef37c177a900ff307
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-02-03 21:45:59 +0530

    updated note.jsx

--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,9 +1,17 @@
 import React from "react"
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline"
 
 const Note = () => {
   return (
     <>
-      <div></div>
+      <div className="note">
+        <h1>Title</h1>
+        <br />
+        <p>This is the content</p>
+        <button className="btn">
+          <DeleteOutlineIcon className="deleteIcon" />
+        </button>
+      </div>
     </>
   )
 }
