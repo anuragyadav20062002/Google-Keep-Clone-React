378553f1cd291845b6996db4070cba555bf87b7b
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-02-03 22:06:01 +0530

    updated createnote

--- src/Components/CreateNote.jsx
+++ src/Components/CreateNote.jsx
@@ -1,14 +1,47 @@
+/* eslint-disable no-unused-vars */
 import React from "react"
 import Button from "@mui/material/Button"
 import AddIcon from "@mui/icons-material/Add"
+import { useState } from "react"
 
 const CreateNote = () => {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  })
+
+  const inputEvent = (event) => {
+    const value = event.target.value
+    const name = event.target.name
+
+    setNote((oldData) => {
+      return {
+        ...oldData,
+        [name]: value,
+        //checks if the name of field matches that in usestate the value is updated in usestate variable
+      }
+    })
+  }
   return (
     <>
       <div className="main_note">
         <form action="">
-          <input type="text" placeholder="Title" autoComplete="off" />
-          <textarea cols="" rows="" placeholder="Write a note"></textarea>
+          <input
+            type="text"
+            name="title"
+            value={note.title}
+            onChange={inputEvent}
+            placeholder="Title"
+            autoComplete="off"
+          />
+          <textarea
+            cols=""
+            rows=""
+            name="content"
+            value={note.content}
+            onChange={inputEvent}
+            placeholder="Write a note"
+          ></textarea>
           <Button>
             <AddIcon className="plus_sign" />
           </Button>
