78549661edfd56f80d17dfb180135397decc38ea
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-02-03 21:41:39 +0530

    created a new component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
 import CreateNote from "./Components/CreateNote"
+import Note from "./Components/Note"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <Footer />
       <CreateNote />
+      <Note />
     </>
   )
 }
