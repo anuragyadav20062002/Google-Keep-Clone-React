9d053ca0fa39f76c6c6da5c8cea39c496f4eaf4a
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-02-03 21:39:33 +0530

    added material ui for buttons

--- src/Components/CreateNote.jsx
+++ src/Components/CreateNote.jsx
@@ -1,13 +1,17 @@
 import React from "react"
 import Button from "@mui/material/Button"
+import AddIcon from "@mui/icons-material/Add"
+
 const CreateNote = () => {
   return (
     <>
-      <div className="main_node">
+      <div className="main_note">
         <form action="">
-          <input type="text" placeholder="Title" />
+          <input type="text" placeholder="Title" autoComplete="off" />
           <textarea cols="" rows="" placeholder="Write a note"></textarea>
-          <button></button>
+          <Button>
+            <AddIcon className="plus_sign" />
+          </Button>
         </form>
       </div>
     </>
